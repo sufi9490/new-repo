e341f697fbf2d8a531d178d57502d752b0aa1722
sufi9490 <43349151+sufi9490@users.noreply.github.com>
2021-05-24 10:34:09 +0530

    Update ec2.tf
    changed key name

--- ec2.tf
+++ ec2.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "my-instance" {
 
   instance_type = var.instance_type
   
-  key_name      = "demo-tcs-key"
+  key_name      = "jen"
  subnet_id = "${aws_subnet.prod-subnet-public-1.id}"
  vpc_security_group_ids = ["${aws_security_group.ssh-allowed.id}"]
 
